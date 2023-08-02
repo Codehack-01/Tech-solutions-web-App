2e22df942e6a4298cf62434e7aa6c04ac0eec0da
Codehack <ademolaprojectdreams@gmail.com>
2023-08-02 03:37:24 +0100

    commits

---
+++ typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-input", {
+    strings: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Acauctor augue mauris augue. Turpis in eu mi bibendum neque egestascongue quisque egestas. Amet porttitor eget dolor morbi non arcu.Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sit amet risus nullam eget felis eget. Turpis massa sed"],
+    typeSpeed: 30,
+    backSpeed: 100,
+    loop: false,
+
+});
